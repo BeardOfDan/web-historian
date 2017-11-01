82a0d4800c9bc0e567408819321f9a2ecd64d31b
Hack Reactor Student <student@hackreactor.com>
2017-11-01 16:53:08 -0700

    Return 404 for invalid urls

--- web/request-handler.js
+++ web/request-handler.js
@@ -7,7 +7,7 @@ const http = require('http');
 
 exports.handleRequest = function (req, res) {
   //res.end(archive.paths.list);
-  console.log("req.url inside handleRequest " + req.url + " req.method " + req.method);
+  console.log('req.url inside handleRequest' + req.url + 'req.method' + req.method);
   var statusCode = 404;
   var headers = {};
   headers['Content-type'] = 'text/html';
@@ -32,46 +32,86 @@ exports.handleRequest = function (req, res) {
       statusCode = 200;
       res.writeHead(statusCode, headers);
       const url = req.url.slice(1);
-      fs.readFile(archive.paths.archivedSites + '/' + url, (error, data) => {
-        if (error) {
-          fs.readFile(archive.paths.list, 'utf-8', (error, data) => {
+      var options = {
+        method: 'HEAD',
+        host: url,
+        port: 80,
+        path: '/'
+      };
+
+
+      // try
+
+      const request = http.request(options, (r)=> {
+        
+        // if r.headers good
+          // do everything else
+        // else
+          // return 404
+        if (r.headers !== undefined) {
+        
+          fs.readFile(archive.paths.archivedSites + '/' + url, (error, data) => {
             if (error) {
-              // this file should already exitst
-              console.log('Something is wrong');
-            }
-            // console.log(data.length, 'data length');
-            data = data.split('/n');
-            if (data.includes(url)) {
-              // make the archive file
-              // then remove this url from the file
-
-
-              const options = {
-                host: url, 
-                port: 80,
-                path: '/'
-              };
-              http.get(options, (res)=>{
-                console.log('response object inside get request');
-                console.log(res);
-
-              }).on('error', (e)=> {
-                console.log(e, 'error!');  
-              });
-            } else {
-              // first time this url is requested
-              // add the url to the file
-              fs.appendFile(archive.paths.list, '/n' + url, (error)=> {
+              fs.readFile(archive.paths.list, 'utf-8', (error, data) => {
                 if (error) {
-                  console.log('error!');
+                  // this file should already exitst
+                  console.log('Something is wrong');
+                }
+                // console.log(data.length, 'data length');
+                data = data.split('\n');
+                if (data.includes(url)) {
+                  // make the archive file
+                  // then remove this url from the file
+
+
+                  const options = {
+                    host: url, 
+                    port: 80,
+                    path: '/'
+                  };
+                  http.get(options, (res)=>{
+                    console.log('response object inside get request');
+                    console.log(res);
+
+                  }).on('error', (e)=> {
+                    console.log(e, 'error!');  
+                  });
+                } else {
+                  // first time this url is requested
+                  // add the url to the file
+                  fs.appendFile(archive.paths.list, '\n' + url, (error)=> {
+                    if (error) {
+                      console.log('error!');
+                      console.log('headers', r.headers);
+                    }
+                  });
                 }
               });
+            } else {
+              res.end(data);
             }
           });
         } else {
-          res.end(data);
+          console.log('invalid url');
         }
+
+
       });
+      request.on('error', ()=> {
+        res.writeHead(404, headers);
+        res.end('Your url is invalid');
+
+      });
+    
+      request.end();
+
+   
+      
+ 
+      // check if the url corresponds to a live website
+      
+
+      
     }
   } else if (req.method === 'POST') {
     statusCode = 201;
