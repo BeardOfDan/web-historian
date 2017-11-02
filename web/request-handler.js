fda27e97e0837cc5cf3295230883ce116006f2d5
Hack Reactor Student <student@hackreactor.com>
2017-11-01 17:34:50 -0700

    Added and Broke some functionality

--- web/request-handler.js
+++ web/request-handler.js
@@ -32,85 +32,9 @@ exports.handleRequest = function (req, res) {
       statusCode = 200;
       res.writeHead(statusCode, headers);
       const url = req.url.slice(1);
-      var options = {
-        method: 'HEAD',
-        host: url,
-        port: 80,
-        path: '/'
-      };
-
-
-      // try
-
-      const request = http.request(options, (r)=> {
-        
-        // if r.headers good
-          // do everything else
-        // else
-          // return 404
-        if (r.headers !== undefined) {
-        
-          fs.readFile(archive.paths.archivedSites + '/' + url, (error, data) => {
-            if (error) {
-              fs.readFile(archive.paths.list, 'utf-8', (error, data) => {
-                if (error) {
-                  // this file should already exitst
-                  console.log('Something is wrong');
-                }
-                // console.log(data.length, 'data length');
-                data = data.split('\n');
-                if (data.includes(url)) {
-                  // make the archive file
-                  // then remove this url from the file
-
-
-                  const options = {
-                    host: url, 
-                    port: 80,
-                    path: '/'
-                  };
-                  http.get(options, (res)=>{
-                    console.log('response object inside get request');
-                    console.log(res);
-
-                  }).on('error', (e)=> {
-                    console.log(e, 'error!');  
-                  });
-                } else {
-                  // first time this url is requested
-                  // add the url to the file
-                  fs.appendFile(archive.paths.list, '\n' + url, (error)=> {
-                    if (error) {
-                      console.log('error!');
-                      console.log('headers', r.headers);
-                    }
-                  });
-                }
-              });
-            } else {
-              res.end(data);
-            }
-          });
-        } else {
-          console.log('invalid url');
-        }
-
-
-      });
-      request.on('error', ()=> {
-        res.writeHead(404, headers);
-        res.end('Your url is invalid');
-
-      });
+      
     
-      request.end();
-
-   
-      
- 
-      // check if the url corresponds to a live website
-      
-
+      // used to have the thing here
       
     }
   } else if (req.method === 'POST') {
@@ -119,15 +43,100 @@ exports.handleRequest = function (req, res) {
     req.on('data', (data)=>{
       body += data;
     });
-    req.on('end', ()=>{
+    req.on('end', () => {
       var url = querystring.parse(body).url;
-      
+    
+      var options = {
+        method: 'HEAD',
+        host: url,
+        port: 80,
+        path: '/'
+      };
+
+      // check if the url is live
+      const request = http.request(options, (r)=> {  
+      // if the url is live, then no error will be thrown   
+
+        console.log('about to read the file ' + archive.paths.archivedSites);
+   
+        fs.readFile(archive.paths.archivedSites + '/' + url, (error, data) => {
+          if (error) { // if this file does not yet exist
+            fs.readFile(archive.paths.list, 'utf-8', (error, data) => {
+
+              console.log('read the file archive.paths.list');
+
+              if (error) {
+                // this file should already exitst
+                console.log('Something is wrong');
+              }
+              data = data.split('\n');
+              if (data.includes(url)) {
+
+                console.log('the file includes ' + url);
+
+                const options = {
+                  host: url, 
+                  port: 80,
+                  path: '/'
+                };
+            
+                http.get(options, (thisResponse)=>{
+                  // console.log('response object inside get request');
+                  // console.log(res);
+                  let body = '';
+
+                  thisResponse.on('data', function(data) {
+                    body += data;
+                  });
+                  thisResponse.on('end', function() {
+                    res.end(body);
+                  });
+                }).on('error', (e)=> {
+                  console.log(e, 'error!');  
+                });
+
+              } else {
+
+                console.log('about to append ' + url + ' to the file');
+
+                // first time this url is requested
+                // add the url to the file
+                fs.appendFile(archive.paths.list, '\n' + url, (error)=> {
+                  if (error) {
+                    console.log('error!');
+                    console.log('headers', r.headers);
+                  } else {
+                    console.log('Added ' + url + ' to the list');
+                  }
+                });
+              }
+            });
+          } else { // if the file already exists, send it to the user
+            res.end(data);
+          }
+        });
+
+      });
+      request.on('error', (e)=> {
+        // console.log('url', url);
+        // console.log('e', e);
+        res.writeHead(404, headers);
+        res.end('Your url is invalid');
+
+      });
+    
+      request.end();  
+
     });
 
+
     
-    statusCode = 200;
-    res.writeHead(statusCode, headers);
-    var readStream = fs.createReadStream(archive.paths.siteAssets + '/index.html');
-    readStream.pipe(res);  
+
+
+
+    // statusCode = 200;
+    // res.writeHead(statusCode, headers);
+    // var readStream = fs.createReadStream(archive.paths.siteAssets + '/index.html');
+    // readStream.pipe(res);  
   }
 };
